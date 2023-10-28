872ef8c69b67a2071d9d0653d6a379c6c79ed8cc
Dhorner531 <raidersbite@gmail.com>
2023-10-28 15:02:33 -0600

    first commit

---
+++ DU-VIRT-FSF-PT-10-2023-U-LOLC-main/03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,10 @@
+// Write Your JavaScript Code Here
+var personName;
+
+var personName = "Dan";
+var pets = "2";
+var funFacts = "My dogs are both over 100bls";
+
+console.log('My nane is ' + personName + '.');
+console.log('I have ' + pets + ' pet(s).');
+console.log('Fun Fact: ' + funFacts);
